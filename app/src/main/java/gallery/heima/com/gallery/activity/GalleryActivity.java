fa5bd78f0b607369235fce799d05662b28fc0c27
Android <Android>
2016-10-12 02:46:58 +0800

    Gallery第二页面的实现

--- app/src/main/java/gallery/heima/com/gallery/activity/GalleryActivity.java
+++ app/src/main/java/gallery/heima/com/gallery/activity/GalleryActivity.java
@@ -1,81 +1,67 @@
 package gallery.heima.com.gallery.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import gallery.heima.com.gallery.R;
-import network.GalleryHomeBean;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
+import network.GalleryBean;
 import network.NetworkManager;
 import network.SexyGirlRequest;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by hasee on 2016/10/12.
+ */
 
-    @BindView(R.id.main_listview)
+public class GalleryActivity extends AppCompatActivity {
+    @BindView(R.id.grllery_list_view)
     ListView mListView;
-
-    private List<GalleryHomeBean.TngouBean> mData;
+    private List<GalleryBean.TngouBean> mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gallery);
         ButterKnife.bind(this);
 
         loadServiceData();
     }
 
-    /**
-     * 发送网络数据
-     */
     private void loadServiceData() {
-        String url = "http://apis.baidu.com/tngou/gallery/classify";
-        SexyGirlRequest request = new SexyGirlRequest(url, GalleryHomeBean.class, mSuccessListener, mErrorListener);
+        int id = getIntent().getIntExtra("id", 0);
+        String url = "http://apis.baidu.com/tngou/gallery/list?id=" + id + "&page=1&rows=20";
+        SexyGirlRequest request = new SexyGirlRequest(url, GalleryBean.class, mSuccessListener, mErrorListener);
         NetworkManager.sendRequest(request);
     }
 
-    private Response.Listener mSuccessListener = new Response.Listener<GalleryHomeBean>() {
+    private Response.Listener mSuccessListener = new Response.Listener<GalleryBean>() {
         @Override
-        public void onResponse(GalleryHomeBean response) {
+        public void onResponse(GalleryBean response) {
             mData = response.getTngou();
             mListView.setAdapter(mBaseAdapter);
-            mListView.setOnItemClickListener(mOnItemClickListener);
-        }
-    };
-
-    
-
-    private AdapterView.OnItemClickListener mOnItemClickListener = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            // 跳转页面
-            int mainId = mData.get(position).getId();
-            Intent intent = new Intent(MainActivity.this,GalleryActivity.class);
-            intent.putExtra("id",mainId);
-            startActivity(intent);
-
         }
     };
 
     private Response.ErrorListener mErrorListener = new Response.ErrorListener() {
         @Override
         public void onErrorResponse(VolleyError error) {
-            Toast.makeText(MainActivity.this,"网络错误",Toast.LENGTH_SHORT).show();
+            Toast.makeText(GalleryActivity.this, "网络错误", Toast.LENGTH_LONG).show();
         }
     };
 
@@ -89,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public GalleryHomeBean.TngouBean getItem(int position) {
+        public GalleryBean.TngouBean getItem(int position) {
             return mData.get(position);
         }
 
@@ -102,23 +88,28 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             ViewHolder holder;
             if (convertView == null) {
-                convertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.list_item_gallery_home, null);
+                convertView = LayoutInflater.from(GalleryActivity.this).inflate(R.layout.list_item_gallery, null);
                 holder = new ViewHolder(convertView);
                 convertView.setTag(holder);
             } else {
                 holder = (ViewHolder) convertView.getTag();
             }
 
-            GalleryHomeBean.TngouBean bean = getItem(position);
-            holder.mTextView.setText(bean.getTitle());
+            GalleryBean.TngouBean bean = getItem(position);
+            holder.mDes.setText(bean.getTitle());
+
+            String imageUrl = "http://tnfs.tngou.net/image" + bean.getImg();
+            Glide.with(GalleryActivity.this).load(imageUrl).bitmapTransform(new CropCircleTransformation(GalleryActivity.this)).into(holder.mIcon);
+
             return convertView;
         }
     };
 
     class ViewHolder {
-
-        @BindView(R.id.home_tv_title)
-        TextView mTextView;
+        @BindView(R.id.tv_des)
+        TextView mDes;
+        @BindView(R.id.iv_icon)
+        ImageView mIcon;
 
         public ViewHolder(View root) {
             ButterKnife.bind(this, root);
